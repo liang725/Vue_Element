df74b41d9648ac1731944d1811ac09a0f74268ee
liang725 <2090809185@qq.com>
2025-11-06 20:54:50 +0800

    Initial commit: Vue project

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!-- <h1 id="htitle">{{ message }}</h1>
+    <TestButton /> -->
+    <!-- <user-view></user-view> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+//import TestButton from './views/element/TestButton.vue';
+//import UserView from './views/element/UserView.vue';
+// import UserView from './views/element/QuestionView.vue';
+
+export default {
+  // components: {
+  //   //TestButton,
+  //   //UserView,
+  // },
+  data() {
+    return {
+      message: "hello vue",
+    };
+  },
+};
+</script>
+
+<style>
+#htitle {
+  color: rgb(143, 46, 46);
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import UserView from '../views/element/UserView.vue'
+import QuestionView from '@/views/element/QuestionView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect: "/user"
+  },
+  {
+    path: '/user',
+    name: 'user',
+    component: UserView
+  },
+    {
+    path: '/question',
+    name: 'question',
+    component: QuestionView
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
